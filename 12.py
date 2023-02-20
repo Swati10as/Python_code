ad8d40d0019ac011bf157ec52018ad0cad8dbed7
swati10as <swatisingh.10as@gmail.com>
2023-02-20 12:00:45 +0530

    latest

--- 12.py
+++ 12.py
@@ -1,3 +1,18 @@
 #Write a function that indicates the number of one-letter words, two-letter words, three-letter words, etc. 
 # For example, “I love Python programming” has zero one-letter words, two two-letter words, 
 # one three-letter word, zero four- and five-letter words, one six-letter word, and one word with more than six letters.
+from num2words import num2words
+s=input("Enter sentence:  ").split(' ')
+d={1:0,2:0,3:0,4:0,5:0,6:0,7:0}
+for i in s:
+    if len(i) in d and len(i)<7:
+
+        d[len(i)]=d[len(i)]+1
+    if len(i)>6:
+        d[7]=d[7]+1
+for i in d.keys():
+    if i!=7:
+        print(num2words(d[i])," ",num2words(i),"letter word ,",end="")
+    else:
+        print(num2words(d[i])," word with more than six letter word")
+    
---
+++ 13.py
@@ -0,0 +1,7 @@
+#Write a function that returns the sum of the first N natural numbers.
+
+n= int(input("Enter n :"))
+
+s=n*(n+1)//2
+
+print(s)
